efaeb2a09640190e7ae11e0e78d68498f6173255
Juan Pablo Contreras <juan.contreras-p@mail.escuelaing.edu.co>
2025-09-11 23:33:50 -0500

    Parcial parte 1 - initializing

---
+++ Parcial-Corte-1-CVDS-DOSW/src/main/java/eci/edu/dosw/parcial/Parcial_Corte_1_CVDS_DOSW/ParcialCorte1CvdsDoswApplication.java
@@ -0,0 +1,13 @@
+package eci.edu.dosw.parcial.Parcial_Corte_1_CVDS_DOSW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialCorte1CvdsDoswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialCorte1CvdsDoswApplication.class, args);
+	}
+
+}
---
+++ Parcial-Corte-1-CVDS-DOSW/src/test/java/eci/edu/dosw/parcial/Parcial_Corte_1_CVDS_DOSW/ParcialCorte1CvdsDoswApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.dosw.parcial.Parcial_Corte_1_CVDS_DOSW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialCorte1CvdsDoswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
